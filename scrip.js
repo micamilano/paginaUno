73a16ea1121cecf2bb61d24e61355fb2a9663792
micamilano <79947300+micamilano@users.noreply.github.com>
2021-10-03 20:51:49 -0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,6 @@
+let menu= document.getElementById('menu');
+let menuBar= document.getElementById('menu-bar');
+
+menuBar.addEventListener('click', function () {
+    menu.classList.toggle('menu-toggle');
+})
